ae527884714c526a25c81b3d64da06711f7748e9
lurumad <luisruizpavon@gmail.com>
2024-06-05 20:04:51 +0200

    refactor: extract method

--- 11-gilded/src/test/java/org/kata/GildedRoseApprovalsTest.java
+++ 11-gilded/src/test/java/org/kata/GildedRoseApprovalsTest.java
@@ -4,7 +4,7 @@ import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 import org.lambda.functions.Function3;
 
-public class GildedRoseApprovals {
+public class GildedRoseApprovalsTest {
 
 	Function3<String, Integer, Integer, String> doUpdateQuality = (name, sellIn, quality) -> {
 		GildedRose app = new GildedRose(new Item[]{new Item(name, sellIn, quality)});
@@ -14,10 +14,10 @@ public class GildedRoseApprovals {
 
 	@Test
 	void coverAll() {
-		// String[] itemNames = {"Aged Brie", "foo", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
-		// Integer[] sellInValues = {-1, 0, 1, 6, 11};
-		// Integer[] qualityValues = {-1, 0, 1, 49, 50};
+		String[] itemNames = {"Aged Brie", "foo", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
+		Integer[] sellInValues = {-1, 0, 1, 6, 11};
+		Integer[] qualityValues = {-1, 0, 1, 49, 50};
 
-		// CombinationApprovals.verifyAllCombinations(doUpdateQuality, itemNames, sellInValues, qualityValues);
+		CombinationApprovals.verifyAllCombinations(doUpdateQuality, itemNames, sellInValues, qualityValues);
 	}
 }
