1d331ec9f603117b647d02f0d38d0c2fa2f9a7fc
lurumad <luisruizpavon@gmail.com>
2024-06-12 20:24:41 +0200

    refactor: remove magic string

--- 14-smelly/src/main/java/org/kata/Game.java
+++ 14-smelly/src/main/java/org/kata/Game.java
@@ -2,7 +2,8 @@ package org.kata;
 
 // Large class
 public class Game {
-    private char _lastSymbol = ' ';
+    public static final char EMPTY = ' ';
+    private char _lastSymbol = EMPTY;
     private Board _board = new Board();
 
     // Data clumps (x, y)
@@ -21,7 +22,7 @@ public class Game {
     }
 
     private void validatePosition(int x, int y) throws Exception {
-        if (_board.TileAt(x, y).Symbol != ' ') {
+        if (_board.TileAt(x, y).Symbol != EMPTY) {
             throw new Exception("Invalid position");
         }
     }
@@ -33,7 +34,7 @@ public class Game {
     }
 
     private void validateFirstMove(char symbol) throws Exception {
-        if (_lastSymbol == ' ') {
+        if (_lastSymbol == EMPTY) {
             //if player is X
             if (symbol == 'O') {
                 throw new Exception("Invalid first player");
@@ -47,9 +48,9 @@ public class Game {
         //if the positions in first row are taken
         // Inappropriate Intimacy
         // Feature Envy
-        if (_board.TileAt(0, 0).Symbol != ' ' &&
-                _board.TileAt(0, 1).Symbol != ' ' &&
-                _board.TileAt(0, 2).Symbol != ' ') {
+        if (_board.TileAt(0, 0).Symbol != EMPTY &&
+                _board.TileAt(0, 1).Symbol != EMPTY &&
+                _board.TileAt(0, 2).Symbol != EMPTY) {
             //if first row is full with same symbol
             if (_board.TileAt(0, 0).Symbol ==
                     _board.TileAt(0, 1).Symbol &&
@@ -59,9 +60,9 @@ public class Game {
         }
 
         //if the positions in first row are taken
-        if (_board.TileAt(1, 0).Symbol != ' ' &&
-                _board.TileAt(1, 1).Symbol != ' ' &&
-                _board.TileAt(1, 2).Symbol != ' ') {
+        if (_board.TileAt(1, 0).Symbol != EMPTY &&
+                _board.TileAt(1, 1).Symbol != EMPTY &&
+                _board.TileAt(1, 2).Symbol != EMPTY) {
             //if middle row is full with same symbol
             if (_board.TileAt(1, 0).Symbol ==
                     _board.TileAt(1, 1).Symbol &&
@@ -72,9 +73,9 @@ public class Game {
         }
 
         //if the positions in first row are taken
-        if (_board.TileAt(2, 0).Symbol != ' ' &&
-                _board.TileAt(2, 1).Symbol != ' ' &&
-                _board.TileAt(2, 2).Symbol != ' ') {
+        if (_board.TileAt(2, 0).Symbol != EMPTY &&
+                _board.TileAt(2, 1).Symbol != EMPTY &&
+                _board.TileAt(2, 2).Symbol != EMPTY) {
             //if middle row is full with same symbol
             if (_board.TileAt(2, 0).Symbol ==
                     _board.TileAt(2, 1).Symbol &&
@@ -84,6 +85,6 @@ public class Game {
             }
         }
 
-        return ' ';
+        return EMPTY;
     }
 }
